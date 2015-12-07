93aff87689940dc3abc827a4d5eec3b25e43e7c4
rwsargent <rw.sarge@gmail.com>
2015-12-07 10:36:04 -0700

    put back deleted studentSubmissionRegistry and student

---
+++ src/main/java/autograder/student/Student.java
@@ -0,0 +1,41 @@
+package autograder.student;
+
+import java.io.File;
+
+import autograder.configuration.AssignmentProperties;
+
+public class Student {
+	public File studentDirectory;
+	public String name;
+	public int canvasId;
+	public boolean isLate;
+	
+	AssignmentProperties assignProps;
+
+	public Student() {
+	}
+	
+	public Student(String name, int canvasId) {
+		this.canvasId = canvasId;
+		studentDirectory = new File(String.format("submissions/%s_%d", name, canvasId));
+		studentDirectory.mkdirs();
+		new File(studentDirectory.getAbsolutePath() + "/source").mkdir();
+	}
+	
+	public Student(String name, int canvasId, boolean late) {
+		this(name, canvasId);
+		this.isLate = late;
+	}
+	
+	public String getSourceDirectoryPath() {
+		return studentDirectory.getAbsolutePath() + "/source";
+	}
+	
+	public String toString() {
+		return name;
+	}
+
+	public void createAssignmentProperties() {
+		assignProps = new AssignmentProperties(studentDirectory.getAbsolutePath() + "/assignment.properties");
+	}
+}
