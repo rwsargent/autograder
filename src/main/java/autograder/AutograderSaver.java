e6ac16b8d0aed04eb4af16d7a420afb619868110
rwsargent <rw.sarge@gmail.com>
2017-03-31 11:21:58 -0600

    Added a saver and submission builder. Phase one looks plausible.

---
+++ src/main/java/autograder/AutograderSaver.java
@@ -0,0 +1,49 @@
+package autograder;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import autograder.configuration.Configuration;
+
+public class AutograderSaver {
+	public static final String AUTOGRADER_SAVE_FILENAME = "saved.autograder";
+	
+	private Configuration mConfig;
+	
+	private String assignment;
+	
+	public AutograderSaver(Configuration configuration) {
+		mConfig = configuration;
+	}
+	
+	public void readFile(File saved) {
+		try(FileReader fr = new FileReader(saved);
+				BufferedReader br = new BufferedReader(fr)) {
+			assignment = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getAssignment() {
+		return assignment;
+	}
+
+	public boolean isCurrentAssignemnt() {
+		return mConfig.assignment == assignment;
+	}
+
+	public void writeSavedFile(File outfile) {
+		try(FileWriter fw = new FileWriter(outfile);
+				BufferedWriter bf = new BufferedWriter(fw)) {
+			fw.write(mConfig.assignment);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
