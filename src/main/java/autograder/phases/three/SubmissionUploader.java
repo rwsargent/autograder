eb52c3201ac3987a9129bc0a0077e77485dd0eeb
Ryan S <rw.sarge@gmail.com>
2017-09-06 23:35:56 -0600

    Added functionalinterface annotation. Why not, ya know?

--- src/main/java/autograder/phases/three/SubmissionUploader.java
+++ src/main/java/autograder/phases/three/SubmissionUploader.java
@@ -2,6 +2,7 @@ package autograder.phases.three;
 
 import autograder.student.AutograderSubmission;
 
+@FunctionalInterface
 public interface SubmissionUploader {
 	public void upload(AutograderSubmission submission);
 }
