53b8b2e966b21aa610812d892f99dbfc60166dce
Ryan S <rw.sarge@gmail.com>
2017-10-03 21:06:48 -0600

    Uploader to write metrics to disk.

--- src/main/java/autograder/phases/three/uploaders/AssignmentMetricsUploader.java
+++ src/main/java/autograder/phases/three/uploaders/AssignmentMetricsUploader.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Paths;
+import java.util.Set;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -16,37 +17,47 @@ import org.slf4j.LoggerFactory;
 import com.google.common.io.Files;
 
 import autograder.Constants;
-import autograder.metrics.MissedTestMetric;
+import autograder.metrics.MetricReport;
 import autograder.phases.three.AssignmentUploader;
 import autograder.student.AutograderSubmissionMap;
 
 /**
- * 
+ * Write the basic metrics for an assignment to file.
+ * File is "metrics.txt" under the output directory for this assignment.
  * @author ryansargent
  */
 public class AssignmentMetricsUploader implements AssignmentUploader {
 	
-	private static final String ASSIGNMENT_METRICS_FILENAME = "failedHistogram.txt";
+	private static final String ASSIGNMENT_METRICS_FILENAME = "metrics.txt";
 	private final Logger LOGGER = LoggerFactory.getLogger(AssignmentMetricsUploader.class);
 	private String assignment;
-	private MissedTestMetric missedTests;
+	private Set<MetricReport> metricReports;
 
 	@Inject
-	public AssignmentMetricsUploader(@Named(ASSIGNMENT)String assignment, MissedTestMetric missedTests) {
+	public AssignmentMetricsUploader(@Named(ASSIGNMENT)String assignment, Set<MetricReport> metricReports) {
 		this.assignment = assignment;
-		this.missedTests = missedTests;
-		
+		this.metricReports = metricReports;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void upload(AutograderSubmissionMap submissions) {
 		LOGGER.info("Calculating metrics for " + assignment);
-		String misssedTestSummary = missedTests.getHistogramSummary(submissions);
+		StringBuilder report = new StringBuilder();
+		for(MetricReport metricReport : metricReports) {
+			metricReport.calculateMetrics(submissions);
+			report.append(metricReport.generateMetricReport() + "\n");
+		}
+		if(report.length() != 0 ) {
+			report.setLength(report.length() - 1); // chop off final new line
+		}
 		File outputDir = Paths.get(Constants.ZIPS, assignment).toFile();
 		outputDir.mkdirs();
 		
 		try {
-			Files.write(misssedTestSummary, new File(outputDir, ASSIGNMENT_METRICS_FILENAME), Charset.defaultCharset());
+			Files.write(report.toString(), new File(outputDir, ASSIGNMENT_METRICS_FILENAME), Charset.defaultCharset());
 		} catch (IOException e) {
 			LOGGER.error("Could not write historgram text file for " + assignment, e);
 		}
